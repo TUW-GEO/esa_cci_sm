58cfd88b53c305a9d6160d935788380633db3727
c-hydro <35603167+c-hydro@users.noreply.github.com>
2018-01-23 11:57:19 +0100

    Add files via upload

---
+++ cci/interface.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+import numpy as np
+import os
+
+from pygeobase.io_base import ImageBase, MultiTemporalImageBase
+from pygeobase.object_base import Image
+
+from pynetcf.time_series import GriddedNcOrthoMultiTs
+from pygeogrids.netcdf import load_grid
+
+from datetime import timedelta
+
+from grid import CCI025Cellgrid
+from netCDF4 import Dataset
+
+class CCI_SM_v042_025Img(ImageBase):
+    """
+    Class for reading one ESACCI SM v0.42 nc file in 0.25° grid.
+
+    Parameters
+    ----------
+    filename: string
+        filename of the ESACCI nc file
+    mode: string, optional
+        mode of opening the file, only 'r' is implemented at the moment
+    parameter : string or list, optional
+        one or list of parameters to read, see ESACCI v0.42 documentation for more information
+        Default : 'SoilMoi0_10cm_inst'
+    array_1D: boolean, optional
+        if set then the data is read into 1D arrays. Needed for some legacy code.
+    """
+
+    def __init__(self, filename, mode='r', parameter='sm', array_1D=False):
+        super(CCI_SM_v042_025Img, self).__init__(filename, mode=mode)
+
+        if type(parameter) != list:
+            parameter = [parameter]
+        self.parameters = parameter
+        self.fill_values = np.repeat(9999., 1440 * 120)
+        self.grid = CCI025Cellgrid()
+        self.array_1D = array_1D
+
+    def read(self, timestamp=None):
+
+        # print 'read file: %s' %self.filename
+        # Returns the selected parameters for a gldas image and according metadata
+        return_img = {}
+        return_metadata = {}
+
+        try:
+            dataset = Dataset(self.filename)
+        except IOError as e:
+            print(e)
+            print(" ".join([self.filename, "can not be opened"]))
+            raise e
+
+        param_names = []
+        for parameter in self.parameters:
+            param_names.append(parameter)
+
+        for parameter, variable in dataset.variables.items():
+            if parameter in param_names:
+                param_metadata = {}
+                param_data = {}
+                for attrname in variable.ncattrs():
+                    if attrname in ['long_name', 'units']:
+                        param_metadata.update({str(attrname): getattr(variable, attrname)})
+
+                param_data = dataset.variables[parameter][:]
+                np.ma.set_fill_value(param_data, 9999)
+
+                param_data = np.concatenate((self.fill_values,
+                                             np.ma.getdata(param_data.filled())
+                                             .flatten()))
+
+                return_img.update({str(parameter): param_data[self.grid.activegpis]})
+                return_metadata.update({str(parameter): param_metadata})
+
+                # Check for corrupt files
+                try:
+                    return_img[parameter]
+                except KeyError:
+                    path, thefile = os.path.split(self.filename)
+                    print ('%s in %s is corrupt - filling image with NaN values' % (parameter, thefile))
+                    return_img[parameter] = np.empty(self.grid.n_gpi).fill(np.nan)
+                    return_metadata['corrupt_parameters'].append()
+
+        dataset.close()
+        if self.array_1D:
+            return Image(self.grid.activearrlon,
+                         self.grid.activearrlat,
+                         return_img,
+                         return_metadata,
+                         timestamp)
+        else:
+            for key in return_img:
+                return_img[key] = np.flipud(return_img[key].reshape((720, 1440)))
+
+            return Image(np.flipud(self.grid.activearrlon.reshape((720, 1440))),
+                         np.flipud(self.grid.activearrlat.reshape((720, 1440))),
+                         return_img,
+                         return_metadata,
+                         timestamp)
+
+    def write(self, data):
+        raise NotImplementedError()
+
+    def flush(self):
+        pass
+
+    def close(self):
+        pass
+
+class CCI_SM_v042_025Ds(MultiTemporalImageBase):
+    """
+    Class for reading ESACCI SM v0.42 images in nc format.
+
+    Parameters
+    ----------
+    data_path : string
+        path to the nc files
+    parameter : string or list, optional
+        one or list of parameters to read, see ESACCI SM v0.42 documentation for more information
+        Default : 'sm'
+    array_1D: boolean, optional
+        if set then the data is read into 1D arrays. Needed for some legacy code.
+    """
+
+    def __init__(self, data_path, parameter='sm', array_1D=False):
+
+        ioclass_kws = {'parameter': parameter,
+                       'array_1D': array_1D}
+
+        sub_path = ['%Y']
+        # filename_templ = "GLDAS_NOAH025_3H.A{datetime}.*.nc4"
+        filename_templ = "ESACCI-SOILMOISTURE-L3S-SSMV-PASSIVE-{datetime}-fv04.2.nc"
+        super(CCI_SM_v042_025Ds, self).__init__(data_path, CCI_SM_v042_025Img,
+                                                  fname_templ=filename_templ,
+                                                  datetime_format="%Y%m%d%H%M%S",
+                                                  subpath_templ=sub_path,
+                                                  exact_templ=False,
+                                                  ioclass_kws=ioclass_kws)
+
+    def tstamps_for_daterange(self, start_date, end_date):
+        """
+        return timestamps for daterange,
+
+        Parameters
+        ----------
+        start_date: datetime
+            start of date range
+        end_date: datetime
+            end of date range
+
+        Returns
+        -------
+        timestamps : list
+            list of datetime objects of each available image between
+            start_date and end_date
+        """
+        img_offsets = np.array([timedelta(hours=0),
+                                timedelta(hours=3),
+                                timedelta(hours=6),
+                                timedelta(hours=9),
+                                timedelta(hours=12),
+                                timedelta(hours=15),
+                                timedelta(hours=18),
+                                timedelta(hours=21)])
+
+        timestamps = []
+        diff = end_date - start_date
+        for i in range(diff.days + 1):
+            daily_dates = start_date + timedelta(days=i) + img_offsets
+            timestamps.extend(daily_dates.tolist())
+
+        return timestamps
+
+class CCITs(GriddedNcOrthoMultiTs):
+
+    def __init__(self, ts_path, grid_path=None):
+        if grid_path is None:
+            grid_path = os.path.join(ts_path, "grid.nc")
+
+        grid = load_grid(grid_path)
+        super(CCITs, self).__init__(ts_path, grid)
---
+++ cci/reshuffle.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+# The MIT License (MIT)
+#
+# Copyright (c) 2016, TU Wien
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+Module for a command line interface to convert the ESACCI data into a
+time series format using the repurpose package
+'''
+
+import os
+import sys
+import argparse
+from datetime import datetime
+
+from pygeogrids import BasicGrid
+
+from repurpose.img2ts import Img2Ts
+
+from cci.interface import CCI_SM_v042_025Ds
+
+
+def get_filetype(inpath):
+    '''
+    Tries to find out the file type by searching for
+    grib or nc files two subdirectories into the passed input path.
+    If function fails, grib is assumed.
+
+    Parameters
+    ------------
+    input_root: string
+        input path where ESACCI data was downloaded
+    '''
+
+    onedown = os.path.join(inpath, os.listdir(inpath)[0])
+    twodown = os.path.join(onedown, os.listdir(onedown)[0])
+
+    filelist = []
+    for path, subdirs, files in os.walk(twodown):
+        for name in files:
+            filename, extension = os.path.splitext(name)
+            filelist.append(extension)
+
+    if '.nc4' in filelist and '.grb' not in filelist:
+        return 'netCDF'
+    elif '.grb' in filelist and '.nc4' not in filelist:
+        return 'grib'
+    else:
+        # if file type cannot be detected, guess grib
+        return 'grib'
+
+
+def mkdate(datestring):
+    if len(datestring) == 10:
+        return datetime.strptime(datestring, '%Y-%m-%d')
+    if len(datestring) == 16:
+        return datetime.strptime(datestring, '%Y-%m-%dT%H:%M')
+
+
+def reshuffle(input_root, outputpath,
+              startdate, enddate,
+              parameters,
+              imgbuffer=50):
+    """
+    Reshuffle method applied to ESACCI SM v0.42 data.
+
+    Parameters
+    ----------
+    input_root: string
+        input path where era interim data was downloaded
+    outputpath : string
+        Output path.
+    startdate : datetime
+        Start date.
+    enddate : datetime
+        End date.
+    parameters: list
+        parameters to read and convert
+    imgbuffer: int, optional
+        How many images to read at once before writing time series.
+    """
+
+    input_dataset = CCI_SM_v042_025Ds(input_root, parameters,
+                                             array_1D=True)
+
+    if not os.path.exists(outputpath):
+        os.makedirs(outputpath)
+
+    global_attr = {'product': 'ESACCI'}
+
+    # get time series attributes from first day of data.
+    data = input_dataset.read(startdate)
+    ts_attributes = data.metadata
+    grid = BasicGrid(data.lon, data.lat)
+
+    reshuffler = Img2Ts(input_dataset=input_dataset, outputpath=outputpath,
+                        startdate=startdate, enddate=enddate,
+                        input_grid=grid,
+                        imgbuffer=imgbuffer, cellsize_lat=5.0, cellsize_lon=5.0,
+                        global_attr=global_attr,
+                        zlib=True,
+                        unlim_chunksize=1000,
+                        ts_attributes=ts_attributes)
+    reshuffler.calc()
+
+
+def parse_args(args):
+    """
+    Parse command line parameters for conversion from image to timeseries
+
+    :param args: command line parameters as list of strings
+    :return: command line parameters as :obj:`argparse.Namespace`
+    """
+    parser = argparse.ArgumentParser(
+        description="Convert ESACCI data to time series format.")
+    parser.add_argument("dataset_root",
+                        help='Root of local filesystem where the data is stored.')
+    parser.add_argument("timeseries_root",
+                        help='Root of local filesystem where the timeseries should be stored.')
+    parser.add_argument("start", type=mkdate,
+                        help=("Startdate. Either in format YYYY-MM-DD or YYYY-MM-DDTHH:MM."))
+    parser.add_argument("end", type=mkdate,
+                        help=("Enddate. Either in format YYYY-MM-DD or YYYY-MM-DDTHH:MM."))
+    parser.add_argument("parameters", metavar="parameters",
+                        nargs="+",
+                        help=("Parameters to download in numerical format. e.g."
+                              "sm for Volumetric soil water layer"))
+
+    parser.add_argument("--imgbuffer", type=int, default=50,
+                        help=("How many images to read at once. Bigger numbers make the "
+                              "conversion faster but consume more memory."))
+    args = parser.parse_args(args)
+    # set defaults that can not be handled by argparse
+
+    print("Converting data from {} to {} into folder {}.".format(args.start.isoformat(),
+                                                                 args.end.isoformat(),
+                                                                 args.timeseries_root))
+    return args
+
+
+def main(args):
+    args = parse_args(args)
+
+    reshuffle(args.dataset_root,
+              args.timeseries_root,
+              args.start,
+              args.end,
+              args.parameters,
+              imgbuffer=args.imgbuffer)
+
+
+def run():
+    main(sys.argv[1:])
+
+if __name__ == '__main__':
+    run()
