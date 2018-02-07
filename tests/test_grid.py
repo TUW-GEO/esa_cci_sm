50d80af8472a31ff8e62818ef61a6f256052da6d
Manuel Schmitzer <manuel.schmitzer@tuwien.ac.at>
2018-02-07 14:23:01 +0100

    add grid test

---
+++ tests/test_grid.py
@@ -0,0 +1,12 @@
+from hsaf_cci_042.grid import CCI025Cellgrid
+
+def test_CCI025_cell_grid():
+    hsaf_grid = CCI025Cellgrid()
+    assert hsaf_grid.activegpis.size == 1036800
+    assert hsaf_grid.activegpis[153426] == 153426
+    assert hsaf_grid.activearrcell[153426] == 1409
+    assert hsaf_grid.activearrlat[153426] == -63.375
+    assert hsaf_grid.activearrlon[153426] == 16.625
+
+
+
