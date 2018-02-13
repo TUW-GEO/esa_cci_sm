200647b6e1c2a2924867ac12f8013cb9a467b678
Manuel Schmitzer <manuel.schmitzer@tuwien.ac.at>
2018-02-13 12:26:49 +0100

    reshuffle test update

--- tests/test_reshuffle.py
+++ tests/test_reshuffle.py
@@ -12,9 +12,9 @@ class ReshuffleTest(unittest.TestCase):
 
     def test_reshuffle(self):
         inpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                              "hsaf_cci_042_data", "esa_cci")
-        startdate = "2016-12-27T00:00"
-        enddate = "2016-12-31T00:00"
+                    "hsaf_cci_042_data", "esa_cci_sm_dailyImages", "passive")
+        startdate = "2016-06-06T00:00"
+        enddate = "2016-06-07T00:00"
         parameters = ["sm"]
         ts_path = tempfile.mkdtemp()
         args = [inpath, ts_path, startdate, enddate] + parameters
@@ -24,10 +24,8 @@ class ReshuffleTest(unittest.TestCase):
 
         ds = CCITs(ts_path)
         ts = ds.read(45, 15)
-        print(ts['sm'].values)
-        ts_values_should = np.array([1.70000002e-01, 9.99900000e+03,
-                                        1.59999996e-01, 9.99900000e+03,
-                                        1.70000002e-01],dtype=np.float32)
+        ts_values_should = np.array([1.29999995e-01, 9.99900000e+03],
+                                    dtype=np.float32)
         nptest.assert_allclose(ts['sm'].values,
                                ts_values_should, rtol=1e-5)
 
